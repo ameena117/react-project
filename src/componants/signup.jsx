d0653ab5c6e77a29cafbb1f0d3ef64df37283ad7
ameena117 <ameenajadallah@gmail.com>
2024-03-17 17:16:15 +0200

    signup

--- src/componants/signup.jsx
+++ src/componants/signup.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react"
+
 function Signup() {
+  const [user,setUser] = useState([]);
+  const handleChangeUser = (e) => {
+      const { name, value } = e.target;
+      setUser({
+        ...user,
+        [name]: value,
+      });
+      console.log(user);
+    };
+  const validationOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(user.email);
+    console.log(user);
+  };
   return (
-    <div>signup</div>
+    <>
+      <form>
+        <input type="email" value={user.email} name="email" onChange={(e) => handleChangeUser(e)} />
+        <input type="text" value={user.username} name="username" onChange={(e) => handleChangeUser(e)} />
+        <input type="password" value={user.password} name="password" onChange={(e) => handleChangeUser(e)} />
+        <input type="password" value={user.rePassword} name="rePassword" onChange={(e) => handleChangeUser(e)} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
 

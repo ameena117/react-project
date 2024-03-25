9c49cc8f16c281bc2f95afe81df03623205af905
ameena117 <ameenajadallah@gmail.com>
2024-03-25 23:04:29 +0200

    sigin

--- src/componants/signin.jsx
+++ src/componants/signin.jsx
@@ -1,6 +1,47 @@
+import axios from "axios";
+import { useState } from "react"
+import { mixed, object, string } from 'yup';
+
 function Signin() {
+  const [user, setUser] = useState([]);
+  const handleChangeUser = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+    console.log(user);
+  };
+
+  const validate = async (e) => {
+    const schema = object({
+      email: string().email().required(),
+      password: string().min(6).max(20).required(),
+    })
+    try {
+      await schema.validate(user, { abortEarly: false });
+      return true;
+    }
+    catch (err) {
+      console.log(err.errors);
+      return false;
+    }
+  };
+  const validationOnSubmit = async (e) => {
+    e.preventDefault();
+    const validation = await validate();
+    console.log(validation);
+    const { data } = await axios.post(`${import.meta.env.VITE_URL}/auth/signin`, {email: user.email, password:user.password})
+    console.log(data);
+  };
   return (
-    <div>signin</div>
+    <>
+      <form onSubmit={validationOnSubmit}>
+        <input type="email" name="email" onChange={handleChangeUser} />
+        <input type="password" name="password" onChange={handleChangeUser} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
 
--- src/componants/signup.jsx
+++ src/componants/signup.jsx
@@ -1,27 +1,63 @@
+import axios from "axios";
 import { useState } from "react"
+import { mixed, object, string } from 'yup';
 
 function Signup() {
-  const [user,setUser] = useState([]);
+  const [user, setUser] = useState([]);
   const handleChangeUser = (e) => {
-      const { name, value } = e.target;
-      setUser({
-        ...user,
-        [name]: value,
-      });
-      console.log(user);
-    };
-  const validationOnSubmit = (e) => {
-    e.preventDefault();
-    console.log(user.email);
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
     console.log(user);
+  };
+
+  const handleSetImg = (e) => {
+    const { name, files } = e.target;
+    setUser({
+      ...user,
+      [name]: files[0],
+    });
+    console.log(user);
+  };
+
+  const validate = async (e) => {
+    const schema = object({
+      userName: string().min(6).max(20).required(),
+      email: string().email().required(),
+      password: string().min(6).max(20).required(),
+      image: mixed().required(),
+    })
+    try {
+      await schema.validate(user, { abortEarly: false });
+      return true;
+    }
+    catch (err) {
+      console.log(err.errors);
+      return false;
+    }
+  };
+  const validationOnSubmit = async (e) => {
+    e.preventDefault();
+    const validation = await validate();
+    console.log(validation);
+    const formData = new FormData();
+    formData.append('email', user.email);
+    formData.append('userName', user.userName);
+    formData.append('password', user.password);
+    formData.append('image', user.image);
+
+    const { data } = await axios.post(`${import.meta.env.VITE_URL}/auth/signup`, formData)
+    console.log(data);
   };
   return (
     <>
-      <form>
-        <input type="email" value={user.email} name="email" onChange={(e) => handleChangeUser(e)} />
-        <input type="text" value={user.username} name="username" onChange={(e) => handleChangeUser(e)} />
-        <input type="password" value={user.password} name="password" onChange={(e) => handleChangeUser(e)} />
-        <input type="password" value={user.rePassword} name="rePassword" onChange={(e) => handleChangeUser(e)} />
+      <form onSubmit={validationOnSubmit}>
+        <input type="email" name="email" onChange={handleChangeUser} />
+        <input type="text" name="userName" onChange={handleChangeUser} />
+        <input type="password" name="password" onChange={handleChangeUser} />
+        <input type="file" name="image" onChange={handleSetImg} />
         <button type="submit">Submit</button>
       </form>
     </>
